07a990daee3ce066488fecdd1418da9c5059fd6a
robinmxr <reajulbd786@gmail.com>
2026-01-19 18:13:56 +0600

    Add project files.

---
+++ Mikro.Catalog/src/Mikro.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mikro.Catalog.Service.Dtos;
+
+namespace Mikro.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Gems", "Used to Buy Things", 9.99m, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Tools", "Used to Build thi8ngs", 19.99m, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Weapons", "Used to kill things", 29.99m, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Food", "Used to fill hunger", 39.99m, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            return items;
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItemById(Guid id)
+        {
+            var item = items.FirstOrDefault(item => item.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+            return CreatedAtAction(nameof(GetItemById), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.FirstOrDefault(item => item.Id == id);
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price,
+            };
+            var index = items.IndexOf(existingItem);
+            items[index] = updatedItem; 
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = items.FirstOrDefault(item => item.Id == id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+            items.Remove(existingItem);
+            return NoContent();
+        }
+    }
+}
---
+++ Mikro.Catalog/src/Mikro.Catalog.Service/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(o =>
+    {
+        o.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1");
+        o.RoutePrefix = "swagger";
+    });
+
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+
+
+app.Run();
+
